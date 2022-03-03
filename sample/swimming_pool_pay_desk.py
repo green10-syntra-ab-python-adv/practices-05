4c14143bab30a8ece51fecbdad6021865dd1a15a
HansVdb <hans.vandenbogaerde@gmail.com>
2022-03-03 14:48:18 +0100

    terug juist gezet

--- sample/swimming_pool_pay_desk.py
+++ sample/swimming_pool_pay_desk.py
@@ -9,5 +9,5 @@ class SwimmingPoolPayDesk:
         if age < 0 : raise ValueError
         elif 0 <= age <= 6 : return 1.00
         elif 6 < age <= 16 : return 2.00
-        elif 16 < age <= 65 : return 2.25 # moet 2.50 zijn
+        elif 16 < age <= 65 : return 2.50 # moet 2.50 zijn, opzettelijk fout
         else : return 1.50
